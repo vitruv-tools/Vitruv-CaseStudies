109b71d127cc22807395653f785f3984240e0389
Michael Langhammer <langhammer@kit.edu>
2014-10-31 21:54:31 +0000

    renaming plugin folder

--- edu.kit.ipd.sdq.vitruvius.casestudies.pcmjava.monitorededitor/src/edu/kit/ipd/sdq/vitruvius/framework/model/monitor/ChangeSubmitter.java
+++
@@ -1,9 +0,0 @@
-package edu.kit.ipd.sdq.vitruvius.framework.model.monitor;
-
-import edu.kit.ipd.sdq.vitruvius.framework.contracts.datatypes.Change;
-
-public interface ChangeSubmitter {
-
-    void submitChange(final Change change);
-
-}
