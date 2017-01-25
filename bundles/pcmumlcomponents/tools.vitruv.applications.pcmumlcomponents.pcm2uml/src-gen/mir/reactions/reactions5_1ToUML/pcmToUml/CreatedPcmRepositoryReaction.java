c52e05fa9bae368071fbcc8fbfc0f043ba923da1
Heiko Klare <heiko.klare@kit.edu>
2017-01-25 18:41:35 +0100

    Update PCM-UML transformations according to new reactions syntax.

--- bundles/pcmumlcomponents/tools.vitruv.applications.pcmumlcomponents.pcm2uml/src-gen/mir/reactions/reactions5_1ToUML/pcmToUml/CreatedPcmRepositoryReaction.java
+++ bundles/pcmumlcomponents/tools.vitruv.applications.pcmumlcomponents.pcm2uml/src-gen/mir/reactions/reactions5_1ToUML/pcmToUml/CreatedPcmRepositoryReaction.java
@@ -1,7 +1,6 @@
 package mir.reactions.reactions5_1ToUML.pcmToUml;
 
 import mir.routines.pcmToUml.RoutinesFacade;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.xbase.lib.Extension;
 import org.palladiosimulator.pcm.repository.Repository;
 import tools.vitruv.extensions.dslsruntime.reactions.AbstractReactionRealization;
@@ -9,6 +8,7 @@ import tools.vitruv.extensions.dslsruntime.reactions.AbstractRepairRoutineRealiz
 import tools.vitruv.extensions.dslsruntime.reactions.ReactionExecutionState;
 import tools.vitruv.extensions.dslsruntime.reactions.structure.CallHierarchyHaving;
 import tools.vitruv.framework.change.echange.EChange;
+import tools.vitruv.framework.change.echange.compound.CreateAndInsertRoot;
 import tools.vitruv.framework.change.echange.root.InsertRootEObject;
 import tools.vitruv.framework.userinteraction.UserInteracting;
 
@@ -19,20 +19,21 @@ class CreatedPcmRepositoryReaction extends AbstractReactionRealization {
   }
   
   public void executeReaction(final EChange change) {
-    InsertRootEObject<org.palladiosimulator.pcm.repository.Repository> typedChange = (InsertRootEObject<org.palladiosimulator.pcm.repository.Repository>)change;
+    CreateAndInsertRoot<Repository> typedChange = (CreateAndInsertRoot<Repository>)change;
     mir.routines.pcmToUml.RoutinesFacade routinesFacade = new mir.routines.pcmToUml.RoutinesFacade(this.executionState, this);
     mir.reactions.reactions5_1ToUML.pcmToUml.CreatedPcmRepositoryReaction.ActionUserExecution userExecution = new mir.reactions.reactions5_1ToUML.pcmToUml.CreatedPcmRepositoryReaction.ActionUserExecution(this.executionState, this);
     userExecution.callRoutine1(typedChange, routinesFacade);
   }
   
   public static Class<? extends EChange> getExpectedChangeType() {
-    return InsertRootEObject.class;
+    return CreateAndInsertRoot.class;
   }
   
-  private boolean checkChangeProperties(final InsertRootEObject<Repository> change) {
-    EObject changedElement = change.getNewValue();
-    // Check model element type
-    if (!(changedElement instanceof Repository)) {
+  private boolean checkChangeProperties(final CreateAndInsertRoot<Repository> change) {
+    if (!(change.getCreateChange().getAffectedEObject() instanceof Repository)) {
+    	return false;
+    }
+    if (!(change.getInsertChange().getNewValue() instanceof Repository)) {
     	return false;
     }
     
@@ -40,10 +41,10 @@ class CreatedPcmRepositoryReaction extends AbstractReactionRealization {
   }
   
   public boolean checkPrecondition(final EChange change) {
-    if (!(change instanceof InsertRootEObject<?>)) {
+    if (!(change instanceof CreateAndInsertRoot)) {
     	return false;
     }
-    InsertRootEObject typedChange = (InsertRootEObject)change;
+    CreateAndInsertRoot<Repository> typedChange = (CreateAndInsertRoot<Repository>)change;
     if (!checkChangeProperties(typedChange)) {
     	return false;
     }
@@ -56,8 +57,9 @@ class CreatedPcmRepositoryReaction extends AbstractReactionRealization {
       super(reactionExecutionState);
     }
     
-    public void callRoutine1(final InsertRootEObject<Repository> change, @Extension final RoutinesFacade _routinesFacade) {
-      Repository _newValue = change.getNewValue();
+    public void callRoutine1(final CreateAndInsertRoot<Repository> change, @Extension final RoutinesFacade _routinesFacade) {
+      InsertRootEObject<Repository> _insertChange = change.getInsertChange();
+      Repository _newValue = _insertChange.getNewValue();
       _routinesFacade.createUmlModel(_newValue);
     }
   }
