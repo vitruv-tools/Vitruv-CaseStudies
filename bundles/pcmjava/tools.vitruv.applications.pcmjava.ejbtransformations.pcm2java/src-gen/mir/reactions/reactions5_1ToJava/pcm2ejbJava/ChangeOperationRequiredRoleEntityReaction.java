d559d7620467545f683b0c38a2a24f96cb4d6755
michael.langhammer@kit.edu <michael.langhammer@kit.edu>
2017-02-28 20:26:14 +0100

    initial commit of ejbtransformations from pcm to java

---
+++ bundles/pcmjava/tools.vitruv.applications.pcmjava.ejbtransformations.pcm2java/src-gen/mir/reactions/reactions5_1ToJava/pcm2ejbJava/ChangeOperationRequiredRoleEntityReaction.java
@@ -0,0 +1,78 @@
+package mir.reactions.reactions5_1ToJava.pcm2ejbJava;
+
+import mir.routines.pcm2ejbJava.RoutinesFacade;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.xtext.xbase.lib.Extension;
+import org.palladiosimulator.pcm.core.entity.InterfaceRequiringEntity;
+import org.palladiosimulator.pcm.repository.OperationRequiredRole;
+import tools.vitruv.extensions.dslsruntime.reactions.AbstractReactionRealization;
+import tools.vitruv.extensions.dslsruntime.reactions.AbstractRepairRoutineRealization;
+import tools.vitruv.extensions.dslsruntime.reactions.ReactionExecutionState;
+import tools.vitruv.extensions.dslsruntime.reactions.structure.CallHierarchyHaving;
+import tools.vitruv.framework.change.echange.EChange;
+import tools.vitruv.framework.change.echange.feature.reference.ReplaceSingleValuedEReference;
+import tools.vitruv.framework.userinteraction.UserInteracting;
+
+@SuppressWarnings("all")
+class ChangeOperationRequiredRoleEntityReaction extends AbstractReactionRealization {
+  public ChangeOperationRequiredRoleEntityReaction(final UserInteracting userInteracting) {
+    super(userInteracting);
+  }
+  
+  public void executeReaction(final EChange change) {
+    ReplaceSingleValuedEReference<OperationRequiredRole, InterfaceRequiringEntity> typedChange = (ReplaceSingleValuedEReference<OperationRequiredRole, InterfaceRequiringEntity>)change;
+    OperationRequiredRole affectedEObject = typedChange.getAffectedEObject();
+    EReference affectedFeature = typedChange.getAffectedFeature();
+    InterfaceRequiringEntity oldValue = typedChange.getOldValue();
+    InterfaceRequiringEntity newValue = typedChange.getNewValue();
+    mir.routines.pcm2ejbJava.RoutinesFacade routinesFacade = new mir.routines.pcm2ejbJava.RoutinesFacade(this.executionState, this);
+    mir.reactions.reactions5_1ToJava.pcm2ejbJava.ChangeOperationRequiredRoleEntityReaction.ActionUserExecution userExecution = new mir.reactions.reactions5_1ToJava.pcm2ejbJava.ChangeOperationRequiredRoleEntityReaction.ActionUserExecution(this.executionState, this);
+    userExecution.callRoutine1(affectedEObject, affectedFeature, oldValue, newValue, routinesFacade);
+  }
+  
+  public static Class<? extends EChange> getExpectedChangeType() {
+    return ReplaceSingleValuedEReference.class;
+  }
+  
+  private boolean checkChangeProperties(final EChange change) {
+    ReplaceSingleValuedEReference<OperationRequiredRole, InterfaceRequiringEntity> relevantChange = (ReplaceSingleValuedEReference<OperationRequiredRole, InterfaceRequiringEntity>)change;
+    if (!(relevantChange.getAffectedEObject() instanceof OperationRequiredRole)) {
+    	return false;
+    }
+    if (!relevantChange.getAffectedFeature().getName().equals("requiringEntity_RequiredRole")) {
+    	return false;
+    }
+    if (relevantChange.isFromNonDefaultValue() && !(relevantChange.getOldValue() instanceof InterfaceRequiringEntity)) {
+    	return false;
+    }
+    if (relevantChange.isToNonDefaultValue() && !(relevantChange.getNewValue() instanceof InterfaceRequiringEntity)) {
+    	return false;
+    }
+    return true;
+  }
+  
+  public boolean checkPrecondition(final EChange change) {
+    if (!(change instanceof ReplaceSingleValuedEReference)) {
+    	return false;
+    }
+    getLogger().debug("Passed change type check of reaction " + this.getClass().getName());
+    if (!checkChangeProperties(change)) {
+    	return false;
+    }
+    getLogger().debug("Passed change properties check of reaction " + this.getClass().getName());
+    getLogger().debug("Passed complete precondition check of reaction " + this.getClass().getName());
+    return true;
+  }
+  
+  private static class ActionUserExecution extends AbstractRepairRoutineRealization.UserExecution {
+    public ActionUserExecution(final ReactionExecutionState reactionExecutionState, final CallHierarchyHaving calledBy) {
+      super(reactionExecutionState);
+    }
+    
+    public void callRoutine1(final OperationRequiredRole affectedEObject, final EReference affectedFeature, final InterfaceRequiringEntity oldValue, final InterfaceRequiringEntity newValue, @Extension final RoutinesFacade _routinesFacade) {
+      final OperationRequiredRole requiredRole = affectedEObject;
+      _routinesFacade.removeRequiredRole(requiredRole, oldValue);
+      _routinesFacade.addRequiredRole(requiredRole);
+    }
+  }
+}
---
+++ bundles/pcmjava/tools.vitruv.applications.pcmjava.ejbtransformations.pcm2java/src-gen/mir/reactions/reactions5_1ToJava/pcm2ejbJava/DeletedComponentReaction.java
@@ -0,0 +1,75 @@
+package mir.reactions.reactions5_1ToJava.pcm2ejbJava;
+
+import mir.routines.pcm2ejbJava.RoutinesFacade;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.xtext.xbase.lib.Extension;
+import org.palladiosimulator.pcm.repository.Repository;
+import org.palladiosimulator.pcm.repository.RepositoryComponent;
+import tools.vitruv.extensions.dslsruntime.reactions.AbstractReactionRealization;
+import tools.vitruv.extensions.dslsruntime.reactions.AbstractRepairRoutineRealization;
+import tools.vitruv.extensions.dslsruntime.reactions.ReactionExecutionState;
+import tools.vitruv.extensions.dslsruntime.reactions.structure.CallHierarchyHaving;
+import tools.vitruv.framework.change.echange.EChange;
+import tools.vitruv.framework.change.echange.compound.RemoveAndDeleteNonRoot;
+import tools.vitruv.framework.change.echange.feature.reference.RemoveEReference;
+import tools.vitruv.framework.userinteraction.UserInteracting;
+
+@SuppressWarnings("all")
+class DeletedComponentReaction extends AbstractReactionRealization {
+  public DeletedComponentReaction(final UserInteracting userInteracting) {
+    super(userInteracting);
+  }
+  
+  public void executeReaction(final EChange change) {
+    RemoveEReference<Repository, RepositoryComponent> typedChange = ((RemoveAndDeleteNonRoot<Repository, RepositoryComponent>)change).getRemoveChange();
+    Repository affectedEObject = typedChange.getAffectedEObject();
+    EReference affectedFeature = typedChange.getAffectedFeature();
+    RepositoryComponent oldValue = typedChange.getOldValue();
+    mir.routines.pcm2ejbJava.RoutinesFacade routinesFacade = new mir.routines.pcm2ejbJava.RoutinesFacade(this.executionState, this);
+    mir.reactions.reactions5_1ToJava.pcm2ejbJava.DeletedComponentReaction.ActionUserExecution userExecution = new mir.reactions.reactions5_1ToJava.pcm2ejbJava.DeletedComponentReaction.ActionUserExecution(this.executionState, this);
+    userExecution.callRoutine1(affectedEObject, affectedFeature, oldValue, routinesFacade);
+  }
+  
+  public static Class<? extends EChange> getExpectedChangeType() {
+    return RemoveAndDeleteNonRoot.class;
+  }
+  
+  private boolean checkChangeProperties(final EChange change) {
+    RemoveEReference<Repository, RepositoryComponent> relevantChange = ((RemoveAndDeleteNonRoot<Repository, RepositoryComponent>)change).getRemoveChange();
+    if (!(relevantChange.getAffectedEObject() instanceof Repository)) {
+    	return false;
+    }
+    if (!relevantChange.getAffectedFeature().getName().equals("components__Repository")) {
+    	return false;
+    }
+    if (!(relevantChange.getOldValue() instanceof RepositoryComponent)) {
+    	return false;
+    }
+    return true;
+  }
+  
+  public boolean checkPrecondition(final EChange change) {
+    if (!(change instanceof RemoveAndDeleteNonRoot)) {
+    	return false;
+    }
+    getLogger().debug("Passed change type check of reaction " + this.getClass().getName());
+    if (!checkChangeProperties(change)) {
+    	return false;
+    }
+    getLogger().debug("Passed change properties check of reaction " + this.getClass().getName());
+    getLogger().debug("Passed complete precondition check of reaction " + this.getClass().getName());
+    return true;
+  }
+  
+  private static class ActionUserExecution extends AbstractRepairRoutineRealization.UserExecution {
+    public ActionUserExecution(final ReactionExecutionState reactionExecutionState, final CallHierarchyHaving calledBy) {
+      super(reactionExecutionState);
+    }
+    
+    public void callRoutine1(final Repository affectedEObject, final EReference affectedFeature, final RepositoryComponent oldValue, @Extension final RoutinesFacade _routinesFacade) {
+      String _entityName = oldValue.getEntityName();
+      _routinesFacade.deleteJavaPackage(oldValue, _entityName, "");
+      _routinesFacade.deleteJavaClassifier(oldValue);
+    }
+  }
+}
