efeb343769dd8e53bf7666c8011444126e210a3d
Heiko Klare <heiko.klare@kit.edu>
2017-07-19 15:58:43 +0200

    Add UML-Java application project

---
+++ bundles/umljava/tools.vitruv.applications.umljava/src/tools/vitruv/applications/umljava/UmlJavaApplication.java
@@ -0,0 +1,26 @@
+package tools.vitruv.applications.umljava;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import tools.vitruv.applications.umljava.java2uml.JavaToUmlChangePropagationSpecification;
+import tools.vitruv.applications.umljava.uml2java.UmlToJavaChangePropagationSpecification;
+import tools.vitruv.framework.applications.AbstractVitruvApplication;
+import tools.vitruv.framework.change.processing.ChangePropagationSpecification;
+
+public class UmlJavaApplication extends AbstractVitruvApplication {
+
+	@Override
+	public Set<ChangePropagationSpecification> getChangePropagationSpecifications() {
+		Set<ChangePropagationSpecification> specs = new HashSet<ChangePropagationSpecification>();
+		specs.add(new UmlToJavaChangePropagationSpecification());
+		specs.add(new JavaToUmlChangePropagationSpecification());
+		return specs;
+	}
+
+	@Override
+	public String getName() {
+		return "UML-Java";
+	}
+
+}
