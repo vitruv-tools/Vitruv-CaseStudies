39a9b22103f25c0c2a50f00da247b5e31c4d4055
Lukas Hennig <uhdts@student.kit.edu>
2020-08-25 20:07:44 +0200

    Added a note about the CbsCommonalitiesApplication not returning a usable set of change propagation specifications currently.

--- bundles/commonalities/tools.vitruv.applications.cbs.commonalities/src/tools/vitruv/applications/cbs/commonalities/CbsCommonalitiesApplication.java
+++ bundles/commonalities/tools.vitruv.applications.cbs.commonalities/src/tools/vitruv/applications/cbs/commonalities/CbsCommonalitiesApplication.java
@@ -11,8 +11,10 @@ public class CbsCommonalitiesApplication extends AbstractVitruvApplication {
 	@Override
 	public Set<ChangePropagationSpecification> getChangePropagationSpecifications() {
 		Set<ChangePropagationSpecification> specs = new HashSet<ChangePropagationSpecification>();
-		//specs.add(new UmlToJavaChangePropagationSpecification()); // TODO
-		//specs.add(new JavaToUmlChangePropagationSpecification());
+		// TODO This is not used/supported currently. The Commonalities language should probably generate a combined
+		// change propagation specification for all the partial change propagation specifications it generates via the
+		// Reactions language. For test cases we use the CBSExecutionTestCompiler to create this combined change
+		// propagation specification by manually searching for generated change propagation specification classes.
 		return specs;
 	}
 
