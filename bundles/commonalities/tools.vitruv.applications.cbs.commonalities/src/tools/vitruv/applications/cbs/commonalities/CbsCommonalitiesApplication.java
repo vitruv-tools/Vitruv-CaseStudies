811edd8d8c9a23c2688bc26261056bf905b35557
Joshua Gleitze <dev@joshuagleitze.de>
2021-01-06 17:23:00 +0100

    Introduce manual ChangePropagationSpecifications for Commonalities

--- bundles/commonalities/tools.vitruv.applications.cbs.commonalities/src/tools/vitruv/applications/cbs/commonalities/CbsCommonalitiesApplication.java
+++ bundles/commonalities/tools.vitruv.applications.cbs.commonalities/src/tools/vitruv/applications/cbs/commonalities/CbsCommonalitiesApplication.java
@@ -1,25 +1,25 @@
 package tools.vitruv.applications.cbs.commonalities;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import tools.vitruv.framework.applications.AbstractVitruvApplication;
 import tools.vitruv.framework.change.processing.ChangePropagationSpecification;
 
 public class CbsCommonalitiesApplication extends AbstractVitruvApplication {
+    @Override
+    public Set<ChangePropagationSpecification> getChangePropagationSpecifications() {
+        return Set.of(new CbsCommonalitiesChangePropagationSpecifications.Uml2ObjectOrientedDesign(),
+                new CbsCommonalitiesChangePropagationSpecifications.ObjectOrientedDesign2Uml(),
+                new CbsCommonalitiesChangePropagationSpecifications.Java2ObjectOrientedDesign(),
+                new CbsCommonalitiesChangePropagationSpecifications.ObjectOrientedDesign2Java(),
+                new CbsCommonalitiesChangePropagationSpecifications.Pcm2ComponentBasedSystems(),
+                new CbsCommonalitiesChangePropagationSpecifications.ComponentBasedSystems2Pcm(),
+                new CbsCommonalitiesChangePropagationSpecifications.ObjectOrientedDesign2ComponentBasedSystems(),
+                new CbsCommonalitiesChangePropagationSpecifications.ComponentBasedSystems2ObjectOrientedDesign());
+    }
 
-	@Override
-	public Set<ChangePropagationSpecification> getChangePropagationSpecifications() {
-		Set<ChangePropagationSpecification> specs = new HashSet<ChangePropagationSpecification>();
-		// TODO This is not used/supported currently. The Commonalities language should probably generate a combined
-		// change propagation specification for all the partial change propagation specifications it generates via the
-		// Reactions language. For test cases we use the CBSExecutionTestCompiler to create this combined change
-		// propagation specification by manually searching for generated change propagation specification classes.
-		return specs;
-	}
-
-	@Override
-	public String getName() {
-		return "CBS Commonalities";
-	}
+    @Override
+    public String getName() {
+        return "CBS Commonalities";
+    }
 }
