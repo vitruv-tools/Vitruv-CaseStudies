51c7feeee7f3ea9e23f42a7d874b5f7115c0b09d
Jan Wittler <udhmw@student.kit.edu>
2021-02-13 13:17:09 +0100

    refactored java accessor generation to use correspondences instead of find by name heuristics

---
+++ bundles/tools.vitruv.applications.umljava/src/tools/vitruv/applications/umljava/uml2java/UmlToJavaTag.java
@@ -0,0 +1,10 @@
+package tools.vitruv.applications.umljava.uml2java;
+
+public final class UmlToJavaTag {
+	public static final String GETTER = "getter";
+	public static final String SETTER = "setter";
+	
+	public static final String[] ACCESSORS = {GETTER, SETTER};
+	
+	private UmlToJavaTag() { }
+}
