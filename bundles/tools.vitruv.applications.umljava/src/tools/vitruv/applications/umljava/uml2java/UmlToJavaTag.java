1346a975e3424ff658a13f260e5137927db673c1
Jan Wittler <udhmw@student.kit.edu>
2021-02-23 09:35:50 +0100

    annotated UmlToJavaTag with @Utility

--- bundles/tools.vitruv.applications.umljava/src/tools/vitruv/applications/umljava/uml2java/UmlToJavaTag.java
+++ bundles/tools.vitruv.applications.umljava/src/tools/vitruv/applications/umljava/uml2java/UmlToJavaTag.java
@@ -1,10 +1,11 @@
 package tools.vitruv.applications.umljava.uml2java;
 
-public final class UmlToJavaTag {
+import edu.kit.ipd.sdq.activextendannotations.Utility;
+
+@Utility
+public class UmlToJavaTag {
 	public static final String GETTER = "getter";
 	public static final String SETTER = "setter";
 	
 	public static final String[] ACCESSORS = {GETTER, SETTER};
-	
-	private UmlToJavaTag() { }
 }
