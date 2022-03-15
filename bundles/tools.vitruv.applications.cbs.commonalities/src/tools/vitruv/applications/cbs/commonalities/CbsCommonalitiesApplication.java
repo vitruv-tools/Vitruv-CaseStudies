8b8a8cf6caf73366389d07ea7b09c5a828dd8ce4
Heiko Klare <dev@heiko.klare.info>
2022-03-15 17:06:21 +0100

    Replace custom Commonalities propagation specification by generated one

--- bundles/tools.vitruv.applications.cbs.commonalities/src/tools/vitruv/applications/cbs/commonalities/CbsCommonalitiesApplication.java
+++ bundles/tools.vitruv.applications.cbs.commonalities/src/tools/vitruv/applications/cbs/commonalities/CbsCommonalitiesApplication.java
@@ -2,20 +2,14 @@ package tools.vitruv.applications.cbs.commonalities;
 
 import java.util.Set;
 
+import tools.vitruv.commonalities.CommonalitiesChangePropagationSpecificationProvider;
 import tools.vitruv.framework.applications.AbstractVitruvApplication;
 import tools.vitruv.framework.propagation.ChangePropagationSpecification;
 
 public class CbsCommonalitiesApplication extends AbstractVitruvApplication {
 	@Override
 	public Set<ChangePropagationSpecification> getChangePropagationSpecifications() {
-		return Set.of(new CbsCommonalitiesChangePropagationSpecifications.Uml2ObjectOrientedDesign(),
-			new CbsCommonalitiesChangePropagationSpecifications.ObjectOrientedDesign2Uml(),
-			new CbsCommonalitiesChangePropagationSpecifications.Java2ObjectOrientedDesign(),
-			new CbsCommonalitiesChangePropagationSpecifications.ObjectOrientedDesign2Java(),
-			new CbsCommonalitiesChangePropagationSpecifications.Pcm2ComponentBasedSystems(),
-			new CbsCommonalitiesChangePropagationSpecifications.ComponentBasedSystems2Pcm(),
-			new CbsCommonalitiesChangePropagationSpecifications.ObjectOrientedDesign2ComponentBasedSystems(),
-			new CbsCommonalitiesChangePropagationSpecifications.ComponentBasedSystems2ObjectOrientedDesign());
+		return CommonalitiesChangePropagationSpecificationProvider.getChangePropagationSpecifications();
 	}
 
 	@Override
