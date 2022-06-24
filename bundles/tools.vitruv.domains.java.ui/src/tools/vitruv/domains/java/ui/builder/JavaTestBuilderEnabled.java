95be27ac5e7a6195efae779623de21e5b98e03da
Heiko Klare <dev@heiko.klare.info>
2022-06-24 15:28:09 +0200

    Add Java UI projects from domains repository

---
+++ bundles/tools.vitruv.domains.java.ui/src/tools/vitruv/domains/java/ui/builder/JavaTestBuilderEnabled.java
@@ -0,0 +1,11 @@
+package tools.vitruv.domains.java.ui.builder;
+
+import tools.vitruv.framework.domains.ui.builder.TestBuilderEnabled;
+
+public class JavaTestBuilderEnabled extends TestBuilderEnabled {
+
+    public JavaTestBuilderEnabled() {
+        super(VitruvJavaBuilder.BUILDER_ID);
+    }
+
+}
