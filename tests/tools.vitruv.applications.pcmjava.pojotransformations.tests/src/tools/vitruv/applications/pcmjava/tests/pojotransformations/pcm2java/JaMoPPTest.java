013b8ef47d5fd21c86f4de1c0a99609dccd52276
Heiko Klare <dev@heiko.klare.info>
2022-07-21 13:49:27 +0200

    Remove obsolete PCM-Java tests projects

--- tests/tools.vitruv.applications.pcmjava.pojotransformations.tests/src/tools/vitruv/applications/pcmjava/tests/pojotransformations/pcm2java/JaMoPPTest.java
+++ tests/tools.vitruv.applications.pcmjava.pojotransformations.tests/src/tools/vitruv/applications/pcmjava/tests/pojotransformations/pcm2java/JaMoPPTest.java
@@ -1,4 +1,4 @@
-package tools.vitruv.applications.pcmjava.tests.transformations;
+package tools.vitruv.applications.pcmjava.tests.pojotransformations.pcm2java;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
