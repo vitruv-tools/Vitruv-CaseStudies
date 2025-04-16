a7accf6d0299c4b7757e1aa620eb77b5dbf4e6ad
Muhammad-Tahir-KIT <muhammad.tahir@kit.edu>
2025-04-16 11:23:26 +0200

    Create UmlToJavaChangePropagationSpecification.java

---
+++ umljava/src/main/java/tools/vitruv/applications/umljava/UmlToJavaChangePropagationSpecification.java
@@ -0,0 +1,5 @@
+package tools.vitruv.applications.umljava;
+
+@SuppressWarnings("all")
+public class UmlToJavaChangePropagationSpecification extends mir.reactions.umlToJava.UmlToJavaChangePropagationSpecification {
+}
