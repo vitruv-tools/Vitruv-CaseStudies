90a24a547687be187fe601f59058f2d584992436
Muhammad-Tahir-KIT <muhammad.tahir@kit.edu>
2025-04-22 13:05:20 +0200

    Create JavaVisibility.java

---
+++ util.temporary/src/main/java/tools/vitruv/applications/util/temporary/java/JavaVisibility.java
@@ -0,0 +1,19 @@
+package tools.vitruv.applications.util.temporary.java;
+
+/**
+ * Enum for the four possible visibilities in java. We created this
+ * enum because the java metamodell does not have a explicit value for
+ * "package-private".
+ *
+ * @author Fei
+ */
+@SuppressWarnings("all")
+public enum JavaVisibility {
+    PUBLIC,
+
+    PRIVATE,
+
+    PROTECTED,
+
+    PACKAGE;
+}
